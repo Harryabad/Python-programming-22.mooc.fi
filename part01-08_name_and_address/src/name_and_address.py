724fcb1b00049ac1d0497489f1dd2a78c8915c8b
Harry <harryabad94@gmail.com>
2022-07-07 08:34:07 +0100

    Part01

---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,8 @@
+# Write your solution here
+gName = input("Given name:")
+fName = input("Family name:")
+sAddress = input("Street Address:")
+city_and_postcode = input("City and postal code:")
+print(gName + " " + fName)
+print(sAddress)
+print(city_and_postcode)
