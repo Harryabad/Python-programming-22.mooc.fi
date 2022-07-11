b8514adaa0724a762fda6a14e4bc7e85752ef1c9
Harry <harryabad94@gmail.com>
2022-07-11 14:30:32 +0100

    Part 02 now complete

---
+++ Part02/part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -0,0 +1,9 @@
+# Write your solution here
+name = input("Please tell me your name: ")
+
+if name == "Jerry":
+    print("Next please!")
+else:
+    num = float(input("How many portions of soup? "))
+    print(f"The total cost is {(num*5.90)})")
+    print("Next please!")
