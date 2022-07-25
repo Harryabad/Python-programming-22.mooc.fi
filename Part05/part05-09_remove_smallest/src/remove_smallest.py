d68e7d58624a4a422221d92c1d3559a5ac043264
Harry <harryabad94@gmail.com>
2022-07-25 16:25:37 +0100

    ex8-ex13

--- Part05/part05-09_remove_smallest/src/remove_smallest.py
+++ Part05/part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,20 @@
-# Write your solution here
+
+def remove_smallest(numbers: list):
+
+    smallestNum = numbers[0]
+
+    for i in numbers:
+        if i < smallestNum:
+            smallestNum = i
+
+    #print(numbers)
+    numbers.remove(smallestNum)
+
+    #print(smallestNum)
+
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
--- Part05/part05-13_transpose_matrix/src/transpose_matrix.py
+++ Part05/part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,37 @@
 # Write your solution here
+
+def transpose(matrix: list):
+
+    #Below works but bad code
+
+    # matrixT = [ [ 0 for i in range(len(matrix)) ] for j in range(len(matrix)) ]
+
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix)):
+    #         matrixT[i][j] = matrix[j][i]
+
+    # for i in range(len(matrix)):
+    #     for j in range(len(matrix)):
+    #         matrix[i][j] = matrixT[i][j]
+
+
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+
+    
+
+if __name__ == "__main__":
+
+    m = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+
+    print(m)
+    transpose(m)
+    print(m)
