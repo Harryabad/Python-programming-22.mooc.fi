551346df6c5b1de337e59022e945f6305611df80
Harry <harryabad94@gmail.com>
2022-08-01 22:05:36 +0100

    part05 ex21-27 complete

--- Part05/part05-21_movie_database/src/movie_database.py
+++ Part05/part05-21_movie_database/src/movie_database.py
@@ -1,1 +1,26 @@
 # Write your solution here
+
+from cProfile import run
+
+
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+
+    movie = {"name": name,
+            "director": director,
+            "year": year,
+            "runtime": runtime}
+
+    # movie = {}
+    # movie["name"] = name
+    # movie["director"] = director
+    # movie["year"] = year
+    # movie["runtime"] = runtime
+
+
+    database.append(movie)
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
--- Part05/part05-27_letter_square/src/letter_square.py
+++ Part05/part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,24 @@
 # Write your solution here
+
+layers = int(input("Layers: "))
+size = (layers*2)-1
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+matrix = []
+center = size//2
+
+for x in range(size):
+    for y in range(size):
+        distance_center = max(abs(x-center),abs(y-center))
+        letter = letters[distance_center]
+        matrix.append(letter)
+
+result = ''
+for i in matrix:
+    result += i
+#print(result)
+#unflatten = [matrix[i*size: i*size+size] for i in range(size)]
+
+unflatten = [result[i*size: i*size+size] for i in range(size)]
+
+for row in unflatten:
+    print(row)
