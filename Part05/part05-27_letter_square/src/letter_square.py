972b409184bdea8ec3ed931e47ba22309f78fa69
Harry <harryabad94@gmail.com>
2022-08-06 16:13:57 +0100

    how flatten works

--- Part05/part05-27_letter_square/src/letter_square.py
+++ Part05/part05-27_letter_square/src/letter_square.py
@@ -22,3 +22,21 @@ unflatten = [result[i*size: i*size+size] for i in range(size)]
 
 for row in unflatten:
     print(row)
+    
+# matrix = [1,2,3,4,5,6,7,8,9]
+# size = 3
+# unflatten = [result[i*size: i*size+size] for i in range(size)] 
+# ^same as
+# [result[i*size: i*3+3] for i in range(3)]
+# so first iteration where i = 0 will be
+# result[0:3] <-> [1,2,3]
+# next iteration where i = 1 is
+# result[3:6] <-> [4,5,6]
+# and lastly where i = 2
+# result[6:9] <-> [7,8,9]
+# and in the end you have unflatten
+# unflatten = [
+#   [1,2,3],
+#   [4,5,6],
+#   [7,8,9]
+# ]
